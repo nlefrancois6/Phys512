c2c72fd50b00f09ecb08820d3c9355925304065a
Noah LeFrancois <49576860+nlefrancois6@users.noreply.github.com>
2020-09-15 11:04:13 -0400

    Fix error_est, replace df w/np arrays, print output

--- A1Q2.py
+++ A1Q2.py
@@ -12,59 +12,61 @@ from scipy import interpolate
 import pandas as pd
 
 #Load the data and flip the arrays to list data in order of increasing voltage
-#temp, voltage, d = np.loadtxt("lakeshore.csv",delimiter=',',unpack=True)
 df = pd.read_csv("lakeshore.csv")
 df = df.iloc[::-1]
 
 #Rescale the derivative by 0.001 to match the units
 df['Deriv'] = 0.001*df['Deriv']
 
+temp = df['Temp'].to_numpy()
+volt = df['Volt'].to_numpy()
+deriv = df['Deriv'].to_numpy()
+
 #Create a cubic spline to represent the data
 #s=0 prevents smoothing during the fit
-tck = interpolate.splrep(df['Volt'], df['Temp'], s=0)
-
-
-#Initialize array of voltages
-vp = np.linspace(df['Volt'][0],df['Volt'][len(df)-1],1000)
+interp = interpolate.interp1d(volt, temp, kind='cubic')
 
 #Evaluate the interpolant defined by the spline tck at a specified voltage
-def interpolant1(voltage, tck):
-    temp = interpolate.splev(voltage, tck, der=0)
+def f_interp(voltage, interp):
+    temp = interp(voltage)
     return temp
 
-Tp = interpolant1(vp, tck)
+#Estimate the error of our interpolated value
+def error_estimate(voltage_in, interp, temp, volt, deriv):
+    #Get the distances from each point to the voltage of interest
+    volt_distances = np.abs(volt-voltage_in)
+    #Pick the closest point and get its index
+    ind_closest = np.where(volt_distances == np.amin(volt_distances))
+    #Get the voltage, temperature, and derivative at the closest point
+    v_closest = volt_distances[ind_closest[0]]
+    slope_closest = deriv[ind_closest[0]]
+    temp_closest = temp[ind_closest[0]]
 
-#Calculate the error by taking the average of the errors at the 3 closest data points
-def error_estimate1_3ptsStencil(voltage, tck, df):
-    #Find nearest 3 voltages in data set
-    volt_distances = np.abs(df['Volt']-voltage)
-    v_closest = min(volt_distances)
+    #Estimate the temperature by adding the distance from the point times the 
+    #slope at the point to the temperature at the point
+    dist = np.abs(voltage_in-v_closest)
+    temp_est = temp_closest + slope_closest*dist
     
-    #Get the 3 closest data points
-    i_closest = volt_distances[volt_distances==v_closest].index.values
-    i_upper = i_closest + 1
-    i_lower = i_closest - 1
-    
-    closest = df.iloc[i_closest]
-    upper = df.iloc[i_upper]
-    lower = df.iloc[i_lower]
-    
-    #Compute the error at each point and take the average
-    V1err = np.abs(closest.iloc[0]['Temp'] - interpolant1(closest.iloc[0]['Volt'], tck))
-    V2err = np.abs(upper.iloc[0]['Temp'] - interpolant1(upper.iloc[0]['Volt'], tck))
-    V3err = np.abs(lower.iloc[0]['Temp'] - interpolant1(lower.iloc[0]['Volt'], tck))
-    
-    errAvg = (V1err + V2err + V3err)/3
-    
-    return errAvg
+    temp_interp = f_interp(voltage_in, interp)
 
-err = error_estimate1_3ptsStencil(0.9, tck, df)
-print('Interpolation Error: ', err)
+    err_est = np.abs(temp_est-temp_interp)
     
-plt.plot(df['Volt'], df['Temp'], marker='.', markersize = 4, linestyle = '', color = 'red', label = 'Data')
+    return temp_interp, err_est
+
+#Test the interpolation and error estimate functions
+voltage_in = 0.8
+[temp_interp, err] = error_estimate(voltage_in, interp, temp, volt, deriv)
+print("Temperature at {} V is {} +/- {} K".format(voltage_in, temp_interp, err))
+    
+#Initialize array of voltages
+vp = np.linspace(volt[0],volt[-1],1000)
+#Interpolate over these points
+Tp = f_interp(vp, interp)
+
+#Plot the results for the full range of data points
+plt.plot(volt, temp, marker='.', markersize = 4, linestyle = '', color = 'red', label = 'Data')
 plt.plot(vp, Tp, '--', color = 'black', label = 'Interpolant')
 plt.xlabel('Voltage (V)')
 plt.ylabel('Temperature (K)')
 plt.title('Temperature Response of Lakeshore DT-670 Diode')
-    
-    
+plt.legend()
