9734f0a69b3de0e3d67667a7212253dca72e2ade
Noah LeFrancois <49576860+nlefrancois6@users.noreply.github.com>
2020-11-06 11:50:14 -0500

    Add matched filter plots and finish script comments

--- A5/A5_script.py
+++ A5/A5_script.py
@@ -63,7 +63,7 @@ for eventname in events:
     i=i+1
 
 #Suppress the output plots if False
-outputPlots = False
+outputPlots = True
 
 #Parts a),b),c) combined to reduce # of for loops required
 #Hanford model
@@ -81,20 +81,22 @@ for i in range(num_events):
     NP = len(strain_h[i,:])
     xp = np.arange(NP)
     xp=xp-1.0*xp.mean()
-    #Define the window function window(x) = 0.5+0.5cos(pi*x/L)
+    #Define the window function window(x) = 0.5+0.5cos(pi*x/L) 
+    #This is maximum in the middle and goes to zero at the edges
     window_func = 0.5+0.5*np.cos(xp*np.pi/np.max(xp))
     #Multiply both the strain and template function by the window func and normalize
     strainWindowed_h[i,:] = strain_h[i,:]*window_func/np.sqrt(np.mean(window_func**2))
     window_th[i,:] = th[i,:]*window_func/np.sqrt(np.mean(window_func**2))
     
-    #A) Define the noise model (need to explain it. Gaussian filter for smoothing?)
+    #A) Define the noise model by taking the |FT|^2 and smoothing it. To do this, I assumed that 
+    #the noise is uncorrelated and therefore used a gaussian filter as explained in ligo_slides.pdf
     tmp = np.fft.rfft(strainWindowed_h[i,:])*np.conj(np.fft.rfft(strainWindowed_h[i,:]))
     noiseModel_h[i,:] = gaussian_filter(np.abs(tmp),4)
     
     #B) Perform a matched filter search of the event using the provided template
     strainFT = np.fft.rfft(strainWindowed_h[i,:])
     templateFT = np.fft.rfft(window_th[i,:])
-    #explain whitening by dividing by sqrt(noiseModel)
+    #Whiten the power spectrums by dividing by sqrt(noiseModel)
     whiten_template = templateFT/np.sqrt(noiseModel_h[i,:])
     whiten_strain = strainFT/np.sqrt(noiseModel_h[i,:])
     matchedFilter_h[i,:] = np.fft.fftshift(np.fft.irfft(np.conj(whiten_template)*whiten_strain))
@@ -119,6 +121,13 @@ for i in range(num_events):
     SNRPeak_h[i] = np.abs(SNR_h[i,timeSNRPeak_ind])
     print('Hanford Event ' + name[i] + ' SNR = ' + str(SNRPeak_h[i]))
 
+"""
+Output:
+Hanford Event GW150914 SNR = 3.8717314222698023
+Hanford Event LVT151012 SNR = 7.1265800638648376
+Hanford Event GW151226 SNR = 0.35702943231737255
+Hanford Event GW170104 SNR = 0.9341851030838584
+"""
 #Livingston model
 strainWindowed_l = np.zeros((num_events,NP))
 noiseModel_l = np.zeros((num_events,int(NP/2)+1))
@@ -132,20 +141,22 @@ for i in range(num_events):
     NP = len(strain_l[i,:])
     xp = np.arange(NP)
     xp=xp-1.0*xp.mean()
-    #Define the window function window(x) = 0.5+0.5cos(pi*x/L)
+    #Define the window function window(x) = 0.5+0.5cos(pi*x/L) 
+    #This is maximum in the middle and goes to zero at the edges
     window_func = 0.5+0.5*np.cos(xp*np.pi/np.max(xp))
     #Multiply both the strain and template function by the window func and normalize
     strainWindowed_l[i,:] = strain_l[i,:]*window_func/np.sqrt(np.mean(window_func**2))
     window_tl[i,:] = tl[i,:]*window_func/np.sqrt(np.mean(window_func**2))
     
-    #A) Define the noise model (need to explain it. Gaussian filter for smoothing?)
+   #A) Define the noise model by taking the |FT|^2 and smoothing it. To do this, I assumed that 
+    #the noise is uncorrelated and therefore used a gaussian filter as explained in ligo_slides.pdf
     tmp = np.fft.rfft(strainWindowed_l[i,:])*np.conj(np.fft.rfft(strainWindowed_l[i,:]))
     noiseModel_l[i,:] = gaussian_filter(np.abs(tmp),4)
     
     #B) Perform a matched filter search of the event using the provided template
     strainFT = np.fft.rfft(strainWindowed_l[i,:])
     templateFT = np.fft.rfft(window_tl[i,:])
-    #explain whitening by dividing by sqrt(noiseModel)
+    #Whiten the power spectrums by dividing by sqrt(noiseModel)
     whiten_template = templateFT/np.sqrt(noiseModel_l[i,:])
     whiten_strain = strainFT/np.sqrt(noiseModel_l[i,:])
     matchedFilter_l[i,:] = np.fft.fftshift(np.fft.irfft(np.conj(whiten_template)*whiten_strain))
@@ -170,6 +181,14 @@ for i in range(num_events):
     SNRPeak_l[i] = np.abs(SNR_l[i,timeSNRPeak_ind])
     print('Livingston Event ' + name[i] + ' SNR = ' + str(SNRPeak_l[i]))
 
+"""
+Output:
+Livingston Event GW150914 SNR = 0.8969320015056981
+Livingston Event LVT151012 SNR = 5.269947446743081
+Livingston Event GW151226 SNR = 0.3886818863511979
+Livingston Event GW170104 SNR = 0.8276815740714171 
+"""
+
 #C) Calculate the signal to noise ratio for the event combining the two measurements
 SNRPeak_combined = np.zeros(num_events)
 for i in range(num_events):
@@ -181,11 +200,19 @@ for i in range(num_events):
     SNRPeak_combined[i] = SNR_combined[timeSNRPeak_ind]
     print('Combined Event ' + name[i] + ' SNR = ' + str(SNRPeak_combined[i]))
 
+"""
+Output:
+Combined Event GW150914 SNR = 3.8726613349224177
+Combined Event LVT151012 SNR = 7.126583080163526
+Combined Event GW151226 SNR = 0.39294296141353136
+Combined Event GW170104 SNR = 0.9342501207119918
+"""
+
 #D) Compare SNR from the matched filter scatter to the SNR we got from our noise model
 SNR_mfScatter_h = np.zeros(num_events)
 SNR_mfScatter_l = np.zeros(num_events)
 for i in range(num_events):
-    #Not 100% sure if this is the correct way to estimate SNR using scatter but makes sense
+    #Estimate the SNR by taking the maximum signal magnitude divided by the scatter (ie standard deviation) in the signal
     SNR_mfScatter_h[i] = np.max(matchedFilter_h[i,:])/np.std(matchedFilter_h[i,:])
     print('Hanford Event ' + name[i] + '\nMatched Filter Scatter SNR = ' + str(SNR_mfScatter_h[i]) + '\nNoise Model SNR = ' + str(SNRPeak_h[i]))
     SNR_mfScatter_l[i] = np.max(matchedFilter_l[i,:])/np.std(matchedFilter_l[i,:])
@@ -193,7 +220,7 @@ for i in range(num_events):
 
 """
 This indicates that the matched filter scatter method overestimates the SNR compared to our
-analytical noise model method. [explain what this indicates]
+analytical noise model method.
 Output:
 Hanford Event GW150914
 Matched Filter Scatter SNR = 13.488722414326844
@@ -277,7 +304,7 @@ for i in range(num_events):
     SNRPeak = SNRPeak_h[i]
     halfMax_SNR = SNRPeak/2
     halfMax_ind = np.argmin(np.abs(np.abs(SNR_h[i,:])-halfMax_SNR))
-    #Time error is the width at half maximum of the SNR peak
+    #Time error is the full-width at half-maximum of the SNR peak
     tErr_h[i] = 2*np.abs(timeSNRPeak-time[halfMax_ind])
     #Spatial error is the time error times the speed of light (ie the speed of the signal)
     xErr_h[i] = tErr_h[i]*3*10**8
@@ -290,7 +317,7 @@ for i in range(num_events):
     SNRPeak = SNRPeak_l[i]
     halfMax_SNR = SNRPeak/2
     halfMax_ind = np.argmin(np.abs(np.abs(SNR_l[i,:])-halfMax_SNR))
-    #Time error is the width at half maximum of the SNR peak
+    #Time error is the full-width at half-maximum of the SNR peak
     tErr_l[i] = 2*np.abs(timeSNRPeak-time[halfMax_ind])
     #Spatial error is the time error times the speed of light (ie the speed of the signal)
     xErr_l[i] = tErr_l[i]*3*10**8
