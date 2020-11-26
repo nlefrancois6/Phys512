91c9e63fbf0faafd8827fc00a70a5d0f6e5d799e
Noah LeFrancois <49576860+nlefrancois6@users.noreply.github.com>
2020-11-26 14:43:36 -0500

    Finished commenting Q1 and added plots. Probably done A6

--- A6/A6Q1.py
+++ A6/A6Q1.py
@@ -10,10 +10,18 @@ import numpy as np
 from matplotlib import pyplot as plt
 from mpl_toolkits import mplot3d
 
+
+"""
+See attached plots Q1_Cplanes.png, Q1_pythonNoPlanes.png for output screenshots.
+
+The C output can be clearly seen to line up along a set of planes when rotated the right way.
+I was not able to see any such non-random structure in the Numpy output.
+
+I was not able to get the local machine test to work.
+"""
 pyRands = np.loadtxt('rand_points_py.txt').T
 
 cRands = np.loadtxt('rand_points_c.txt').T
-
 
 fig = plt.figure(figsize = (10, 7))
 ax = plt.axes(projection ="3d")
